47df1f0abb05be141bd965fb1271b913c333b187
Kathure <lizkathure@gmail.com>
2016-04-30 22:15:48 +0300

    added comments

--- ex6.py
+++ ex6.py
@@ -1,13 +1,13 @@
-x = "There are %d types of people." %10
-binary = "binary"
-do_not = "don't"
-y = "Those who know %s and those who %s" % (binary, do_not)
+x = "There are %d types of people." %10 #variable x takes in the string statement , %d is a plceholder for the double value 10
+binary = "binary" # variable binary takes in the string "binary"
+do_not = "don't" #variable do_not is assigned the string "don't"
+y = "Those who know %s and those who %s" % (binary, do_not) #variable y is assigned the string statement , %s is a placeholder for sting vars binary and do_not
 
-print x
-print y
+print x #prints vraiable x
+print y #prints variable y
 
-print "I said: %r. " %x
-print "I also said: '%s'." %y
+print "I said: %r. " %x   # prints string x inside another string %r is a placeholder for  string x
+print "I also said: '%s'." %y  #prints string y
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
