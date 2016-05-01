75bd70a403fd89d7e5dee9a63dc0ed1dba9f2a9f
Kathure <lizkathure@gmail.com>
2016-05-01 23:24:08 +0300

    commented each line

--- ex8.py
+++ ex8.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-formatter = "%r %r %r %r"
-print formatter %(1,2,3,4)
-print formatter %("one" , "two","three","four")
-print formatter %(formatter, formatter,formatter,formatter)
-print formatter %(
+formatter = "%r %r %r %r" #variable with 4 placeholders
+print formatter %(1,2,3,4) #prints 4 integers into the placehlder variable declared above
+print formatter %("one" , "two","three","four") #prints four short strings into the placeholder variable
+print formatter %(formatter, formatter,formatter,formatter) #prints the placeholders within themselves
+print formatter %( #prints four longer strings into the placeholder variable
     "I had this thing.", #don't forget the commas
     "That you could type up right." ,
     "But it didn't sing." ,
