29bd44da31526ac23ab8b6d12ed1a7704874ac82
Kathure <lizkathure@gmail.com>
2016-05-01 22:55:21 +0300

    commented what each line does

--- ex7.py
+++ ex7.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
-print "Mary had a little lamb."
-print "It's fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
+print "Mary had a little lamb." #prints statement inside ""
+print "It's fleece was white as %s." % 'snow' #prints string inside string
+print "And everywhere that Mary went." #prints string
 print "." * 10 #prints the dot ten times
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C" #Variable containing char C
+end2 = "h" ##Variable containing char h
+end3 = "e" ##Variable containing char e
+end4 = "e" #Variable containing char e
+end5 = "s" #Variable containing char s
+end6 = "e" #Variable containing char e
+end7 = "B" ##Variable containing char B
+end8 = "u" #Variable containing char u
+end9 = "r" #Variable containing char r
+end10 = "g" #Variable containing char g
+end11 = "e" ##Variable containing char e
+end12 = "r" #Variable containing char r
 
 print end1 + end2 + end3 + end4 +end5 +end6, # comma makes the next string to be printed on the same line
-print end7 + end8 + end9 + end10 + end11 +end12
+print end7 + end8 + end9 + end10 + end11 +end12 #prints second set of variebles
