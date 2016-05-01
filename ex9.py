0c997617488c7731a3d1db0487275801d7d0e07f
Kathure <lizkathure@gmail.com>
2016-05-01 23:36:02 +0300

    commented on each line

---
+++ ex9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+days = "Mon Tue Wed Thu Fri Sat Sun" #var days containing a string
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # variable months containing a string with \n new line thing
+print "Here are the days: ", days #prints string days inside another string on same string
+print "Here are the months: ", months #prints string months inside string, on new lines
+
+print """
+There's something going on here.
+With the three double-qoutes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5,or 6.
+""" #triple quotes make it possible for code to be printed on new lines and to type ax much as you want
