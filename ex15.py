0ee0d9f5f56daee992fa40d217a96aaea67b5004
Kathure <lizkathure@gmail.com>
2016-05-02 19:53:54 +0300

    commented what each line does

--- ex15.py
+++ ex15.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
-from sys import argv
+from sys import argv #imports the argv module
 
-script, filename = argv
+script, filename = argv #unpacks argv so that it gets assigned to variables script and filename
 
-txt = open(filename)
+txt = open(filename) #opens the text file
 
-print "Here's your file %r:" %filename
-print txt.read()
+print "Here's your file %r:" %filename #prints the name of the file
+print txt.read() #prints the content of the text file
 
-print "Type the filename again:"
-file_again = raw_input(">")
 
-txt_again = open(file_again)
+print "Type the filename again:" #prints string
+file_again = raw_input(">") #takes filename
 
-print txt_again.read()
+txt_again = open(file_again) #opens file named above
+
+print txt_again.read() #prints content of file named above
