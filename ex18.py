dfe0abd19a2999bb34c2bc58f53f62dfa18ce287
Kathure <lizkathure@gmail.com>
2016-05-03 23:09:37 +0300

    exercise 8

---
+++ ex18.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+def pritn_two(*args):
+    arg1, arg2 =args
+    print "arg1: %r, arg2: %r" %(arg1, arg2)
+
+def print_two_again(arg1,arg2):
+    print "arg1: %r, arg2: %r" %(arg1,arg2)
+
+def print_one(arg1):
+    print "arg1: %r" % arg1
+
+def print_none():
+    print "I got nothin'."
+
+print_two("Liz","Kathure")
+print_two_again("Liz","Kathure")
+print_one("First!")
+print_none()
