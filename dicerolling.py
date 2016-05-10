1caec37559a0724eee1f6acfa81fdfb75dfcf258
Kathure <lizkathure@gmail.com>
2016-05-10 15:21:25 +0300

    dice rolling simulator

---
+++ dicerolling.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import random
+def rolldice():
+    print "roll the dice "
+    print random.randint(1,6)
+
+
+    def gambling(x):
+        if x == y:
+            print "you have a gambling problem but here goes"
+            rolldice()
+        elif x == n:
+            print "don't quit just yet..... :("
+        else:
+            print " I'll take that as a yes"
+rolldice()
+
